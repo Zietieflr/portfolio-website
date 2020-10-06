2fbf6512a540e83033fb6c3179f5688805531627
Zietieflr <lrmcguire93@gmail.com>
2020-10-05 20:27:24 -0600

    Remove scripts when navigating away from Medium page. Style with Medium page. Add Fontawesome tags to Spell Checker Extension.

--- src/components/Medium.js
+++ src/components/Medium.js
@@ -1,20 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import url from "../helpers/urls";
 
 export default function Medium(){
+  const [articleWidgetWorking, setArticleWidgetWorking] = useState(true)
+
   function initialScript(src) {
-    return new Promise(function(resolve) {
-      const script = document.createElement("script");
-      script.src = src;
-      script.addEventListener("load", () => resolve());
-      document.body.appendChild(script);
+    return new Promise(function(resolve, reject) {
+      const $script = document.createElement("script");
+      $script.id = "mediumScript";
+      $script.src = src;
+      $script.addEventListener("load", resolve);
+      $script.addEventListener("error", reject)
+      document.body.appendChild($script);
+      setArticleWidgetWorking(true);
     })
   };
 
   function renderMedium() {
     const $renderMedium = document.createElement("script");
+    $renderMedium.id = "renderingScript";
     $renderMedium.innerHTML = `MediumWidget.Init({
       renderTo: '#medium-widget',
-      params: {"resource":"https://medium.com/@lrmcguire93",
+      params: {"resource":"${url("medium")}",
       "postsPerLine":1,
       "limit":5,
       "picture":"small",
@@ -25,14 +32,35 @@ export default function Medium(){
     document.body.appendChild($renderMedium);
   }
 
+  function renderError() {
+    setArticleWidgetWorking(false);
+  }
+
   useEffect(() => {    
-    initialScript("https://medium-widget.pixelpoint.io/widget.js").then(renderMedium);
+    initialScript(url("mediumWidget")).then(renderMedium, renderError);
+    return () => {
+      const $rendingScript = document.getElementById("renderingScript");
+      const $mediumScript = document.getElementById("mediumScript");
+      document.body.removeChild($rendingScript);
+      document.body.removeChild($mediumScript);
+    }
   }, []);
+
+  function verifyArticleWidget() {
+    return articleWidgetWorking
+      ? <article id="medium-widget"></article>
+      : <p className="widgetError">
+        Sorry, Medium is being shy and doesn't want to show itself. 
+        You can see my articles <a href={url("medium")}>here.</a>
+      </p>
+  }
 
   return(
     <>
-      <h3>Medium</h3>
-      <article id="medium-widget"></article>
+      <h3>
+        <a href={url("medium")}>Medium</a>
+      </h3>
+      {verifyArticleWidget()}
     </>
   )
 }
--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,5 +1,6 @@
 const urls = {
-
+  medium: "https://medium.com/@lrmcguire93",
+  mediumWidget: "https://medium-widget.pixelpoint.io/widget.js",
 }
 
 export default function url(key) {

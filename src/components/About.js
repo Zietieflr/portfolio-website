ae62006aab54f908d493de942a3ab320c114c64d
Zietieflr <lrmcguire93@gmail.com>
2020-10-14 15:26:08 -0600

    Set component structure for AboutMe. Add image carousel library.

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+import LifeSection from "./LifeSections"
+
 export default function About(){
   return(
-    <h3>About Me</h3>
+    <>
+      <h3>About Me</h3>
+      <LifeSection />
+    </>
   )
 }
---
+++ src/components/LifeSections.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import RotatingImage from "./RotatingImages";
+
+export default function LifeSection() {
+  return (
+    <RotatingImage />
+  )
+}
